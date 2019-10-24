b1dbb3e0a66950fedeb24498026405b2bb77eba7
Damilola Michael Ige <idm.calculus@gmail.com>
2019-10-24 19:59:30 +0100

    specifies data type

--- .history/src/app/product.service_20191024193541.ts
+++ .history/src/app/product.service_20191024193541.ts
@@ -3,15 +3,14 @@ import { Http, Response } from '@angular/http';
 
 import { Album } from './album';
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
-  getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
   };
 
   constructor(private _http: Http) { }
