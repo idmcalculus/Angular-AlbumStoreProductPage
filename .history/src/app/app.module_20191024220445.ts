40da5361d9c1590b8f0217c9746c8aca7c1a6803
Damilola Michael Ige <idm.calculus@gmail.com>
2019-10-24 22:23:42 +0100

    adds routes

--- .history/src/app/app.module_20191024220445.ts
+++ .history/src/app/app.module_20191024220445.ts
@@ -12,9 +12,8 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 
 const appRoutes: Routes = [
-  { path: 'products', component: ProductListComponent },
-  { path: 'product/:id', component: ProductPageComponent },
-  { path: '', redirectTo: 'products', pathMatch: 'full' }
+  { path: 'products', component: ProductListComponent},
+  { path: 'product/:id', component: ProductPageComponent}
 ];
 
 @NgModule({
