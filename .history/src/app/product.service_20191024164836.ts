a3e2880b43940bca3a20a98b2342baacab6a518f
Damilola Michael Ige <idm.calculus@gmail.com>
2019-10-24 17:08:04 +0100

    includes product service

---
+++ .history/src/app/product.service_20191024164836.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from  '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  constructor(
+    private _http: Http,
+    private _albumUrl: '../assets/album.json'
+  ) { }
+
+}
